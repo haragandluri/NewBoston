db2b537da3aa367cc4f53837701b2b7dfdf0439b
Haragandluri <haragandluri@gmail.com>
2019-01-11 12:23:18 +0000

    Hellow World

---
+++ src/YouTube/Apples.java
@@ -0,0 +1,13 @@
+package YouTube;
+
+public class Apples {
+    public static void main(String[] args) {
+        String Name;
+        Name="Hara";
+        System.out.print("hello Worl" +
+                "d");
+        System.out.print("My name is ");
+        System.out.print(Name);
+
+    }
+}
